314aec652354d29deebed1dc24a62b779397bd7f
Yanhao Ding <yding20@ur.rochester.edu>
2018-06-23 10:48:13 +0800

    SecondCommit

--- 5.kdtree/KdTree.java
+++ 5.kdtree/KdTree.java
@@ -9,36 +9,40 @@ import edu.princeton.cs.algs4.RedBlackBST;
 
 public class KdTree {
 	private Node root;
+	private int size;
 
 	private static class Node {
 		private Point2D p;
 		private RectHV rect;
 		private Node lb;
 		private Node rt; 
-		private static int size = 0;
 		// Node class constructor
 		public Node(Point2D p, RectHV rect) {
             this.p = p;
             this.rect = rect;
-            size++;
         }
 	}
 
 	// The constructor
 	public	KdTree() {
-
+		this.size = 0;
+		this.root = null;
 	}
 	public boolean isEmpty() {
 		return size() == 0;
 	}
 	public int size() {
-		return Node.size;
+		return size;
 	}
 
 	public void insert(Point2D p) {
+		if (p == null) throw new IllegalArgumentException("calls get() with a null key");
 		int count = 0;
 		RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
-		root = insert(root, p, count, rect);
+		if (!contains(p)) {
+			root = insert(root, p, count, rect);
+			size++;
+		}
 	}
 
 	private Node insert(Node currentnode, Point2D p, int count, RectHV rect) {
@@ -48,24 +52,41 @@ public class KdTree {
         if (count%2 == 0) {
         	cmp = p.y() - currentnode.p.y();
         	if (cmp < 0) {
-        		rect = new RectHV(currentnode.rect.xmin(), currentnode.rect.ymin(), 
+        		if (currentnode.lb == null) {
+        			rect = new RectHV(currentnode.rect.xmin(), currentnode.rect.ymin(), 
         							currentnode.rect.xmax(), currentnode.p.y());
+        		} else {
+        			rect = currentnode.lb.rect;
+        		}
         		currentnode.lb  = insert(currentnode.lb,  p, count, rect);
         	} else {
-       			rect = new RectHV(currentnode.rect.xmin(), currentnode.p.y(), 
+        		if (currentnode.rt == null) {
+        			rect = new RectHV(currentnode.rect.xmin(), currentnode.p.y(), 
         							currentnode.rect.xmax(), currentnode.rect.ymax());
+        		} else {
+        			rect = currentnode.rt.rect;
+        		}
         		currentnode.rt = insert(currentnode.rt, p, count, rect);
         	}
         	return currentnode;
         } else {
         	cmp = p.x() - currentnode.p.x();
         	if (cmp < 0) {
-        		rect = new RectHV(currentnode.rect.xmin(), currentnode.rect.ymin(), 
+        		if (currentnode.lb == null) {
+        			rect = new RectHV(currentnode.rect.xmin(), currentnode.rect.ymin(), 
         							currentnode.p.x(), currentnode.rect.ymax());
+        		} else {
+        			rect = currentnode.lb.rect;
+        		}
         		currentnode.lb  = insert(currentnode.lb,  p, count, rect);
+
         	} else {
-        		rect = new RectHV(currentnode.p.x(), currentnode.rect.ymin(), 
+        		if (currentnode.rt == null) {
+        			rect = new RectHV(currentnode.p.x(), currentnode.rect.ymin(), 
         							currentnode.rect.xmax(), currentnode.rect.ymax());
+        		} else {
+        			rect = currentnode.rt.rect;
+        		}
         		currentnode.rt = insert(currentnode.rt, p, count, rect);
         	}
         	return currentnode;
@@ -107,7 +128,7 @@ public class KdTree {
 
 	private Node draw(Node root, int count) {
 		count++;
-		RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+		RectHV rect;
 		if (root == null)  return root;
 
 		root.lb = draw (root.lb, count);
@@ -132,6 +153,8 @@ public class KdTree {
 	}
 
 	public Iterable<Point2D> range(RectHV rect) {
+		 if (rect == null)
+             throw new IllegalArgumentException("Point2D p is not illegal!");
 		Stack<Point2D> stack = new Stack<Point2D>();
 		root = range(root, stack, rect);
 		return stack;
@@ -148,57 +171,87 @@ public class KdTree {
 		return currentnode;
 	}
 
-	public Point2D nearest(Point2D p) {
-		Stack<Point2D> stack = new Stack<Point2D>();
-		double distance2 = root.p.distanceSquaredTo(p);
-		stack.push(root.p);
+//	public Point2D nearest(Point2D p) {
+//		if (p == null) throw new NullPointerException("Null point");
+//        if (root == null) return null;
+//        
+//		Stack<Point2D> stack = new Stack<Point2D>();
+//		double distance2 = root.p.distanceSquaredTo(p);
+//		stack.push(root.p);
+//
+//		root = nearest(root, p, stack, distance2);
+//
+//		return stack.pop();
+//	}
+//
+//	private Node nearest(Node currentnode, Point2D p, Stack<Point2D> stack, double distance2) {
+//		if (currentnode == null) return currentnode;
+//		if (currentnode.rect.distanceSquaredTo(p) < distance2) {
+//			if (currentnode.p.distanceSquaredTo(p) < distance2) {
+//				distance2 = currentnode.p.distanceSquaredTo(p);
+//			}
+//			currentnode.lb = nearest(currentnode.lb, p, stack, distance2);
+//			currentnode.rt = nearest(currentnode.rt, p, stack, distance2);
+//			stack.pop();
+//			stack.push(currentnode.p);
+//		}
+//		return currentnode;
+//	}
 
-		root = nearest(root, p, stack, distance2);
+     public Point2D nearest(Point2D p) {
+         // a nearest neighbor in the set to point p; null if the set is empty
+         if (p == null)
+             throw new IllegalArgumentException("Point2D p is not illegal!");
+         if (root != null)
+             return nearest(root, p, root.p);
+         return null;
+     }
 
-		return stack.pop();
-	}
-
-	private Node nearest(Node currentnode, Point2D p, Stack<Point2D> stack, double distance2) {
-		if (currentnode == null) return currentnode;
-		if (currentnode.rect.distanceSquaredTo(p) < distance2) {
-			currentnode.lb = nearest(currentnode.lb, p, stack, distance2);
-			currentnode.rt = nearest(currentnode.rt, p, stack, distance2);
-			if (currentnode.p.distanceSquaredTo(p) < distance2) {
-				distance2 = currentnode.p.distanceSquaredTo(p);
-				stack.pop();
-				stack.push(currentnode.p);
-			}
-		}
-		return currentnode;
-	}
+     private Point2D nearest(Node x, Point2D p, Point2D currNearPoint) {
+         if(x.p.equals(p)) return x.p;
+         double currMinDistance = currNearPoint.distanceTo(p);
+         if (Double.compare(x.rect.distanceTo(p), currMinDistance) >= 0)
+             return currNearPoint;
+         else {
+             double distance = x.p.distanceTo(p);
+             if (Double.compare(x.p.distanceTo(p), currMinDistance) == -1) {
+                 currNearPoint = x.p;
+                 currMinDistance = distance;
+             }
+             if (x.lb != null)
+                 currNearPoint = nearest(x.lb, p, currNearPoint);
+             if (x.rt != null)
+                 currNearPoint = nearest(x.rt, p, currNearPoint);
+         }
+         return currNearPoint;
+     }
 
 	public static void main(String[] args) {
-//		String filename = args[0];
-//        In in = new In(filename);
+		String filename = args[0];
+        In in = new In(filename);
         KdTree kdtree = new KdTree();
-//        while (!in.isEmpty()) {
-//            double x = in.readDouble();
-//            double y = in.readDouble();
-//            Point2D p = new Point2D(x, y);
-//            kdtree.insert(p);
-//        }
-//    
-//    	kdtree.draw();
-//    	RectHV rect = new RectHV(0.0, 0.5, 1.0, 1.0);
-//    	//for (Point2D p : kdtree.range(rect)) {
-//    	//	StdDraw.setPenColor(StdDraw.GREEN);
-//    	//	StdDraw.setPenRadius(0.02);
-//        //	p.draw();
-//    	//}
-//    	Point2D testp = new Point2D(0.81, 0.31);
-//
-//    	StdDraw.setPenColor(StdDraw.RED);
-//    	StdDraw.setPenRadius(0.04);
-//    	testp.draw();
-//
-//    	StdDraw.setPenColor(StdDraw.GREEN);
-//    	StdDraw.setPenRadius(0.04);
-//    	kdtree.nearest(testp).draw();
-		System.out.println(kdtree.size());
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            kdtree.insert(p);
+        }
+    
+    	kdtree.draw();
+    	RectHV rect = new RectHV(0.0, 0.5, 1.0, 1.0);
+    	//for (Point2D p : kdtree.range(rect)) {
+    	//	StdDraw.setPenColor(StdDraw.GREEN);
+    	//	StdDraw.setPenRadius(0.02);
+        //	p.draw();
+    	//}
+    	Point2D testp = new Point2D(0.81, 0.30);
+
+    	StdDraw.setPenColor(StdDraw.RED);
+    	StdDraw.setPenRadius(0.04);
+    	testp.draw();
+
+    	StdDraw.setPenColor(StdDraw.GREEN);
+    	StdDraw.setPenRadius(0.04);
+    	kdtree.nearest(testp).draw();
 	}
 }
--- 5.kdtree/README.md
+++ 5.kdtree/README.md
@@ -1,1 +1,8 @@
 1. First submit 60%
+
+2. Second Submmit 94% 
+Solved wired size problem, when define size as a static field inside class Node,do not work well. Size is 1 before any insert???
+
+Solved the problem that insert calls too many time rect. The last version, create rect for everynode, even if they already exsit, waste lot of time. Change to a if (next = null) create a new rect, else copy the rect and pass to next level. The grade improve a lot by this.
+
+Nearest is still a big problem.
